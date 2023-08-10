b32d9cb8166188e80a98151e616f7b2994d1b9dd
Sedra2000 <$edr@2000D@rsoft>
2023-08-10 16:51:19 +0300

    make custom listItem

--- lib/features/posts/presentation/widgets/posts_page/post_list_widget.dart
+++ lib/features/posts/presentation/widgets/posts_page/post_list_widget.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture_posts_app/core/app_theme.dart';
 import 'package:flutter/material.dart';
 
 import '../../../domain/entities/posts.dart';
@@ -10,33 +11,56 @@ class PostListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
+    return ListView.builder(
         itemBuilder: (context, index) {
-          final post = posts[index];
-          return ListTile(
-            leading: Text(post.id.toString()),
-            title: Text(
-              post.title,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            subtitle: Text(
-              post.body,
-              style: const TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            contentPadding: const EdgeInsets.symmetric(horizontal: 10),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (_) => PostDetailPage(post: posts[index])));
-            },
-          );
-        },
-        separatorBuilder: (context, index) {
-          return const Divider(
-            thickness: 1,
-          );
+          return listItem(context, posts[index]);
         },
         itemCount: posts.length);
+  }
+
+  Widget listItem(BuildContext context, Post post) {
+    const double radius = 16;
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (_) => PostDetailPage(post: post)));
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(radius),
+          ),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: secondaryColor.withOpacity(0.1),
+                  borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(radius),
+                      topLeft: Radius.circular(radius)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    post.title,
+                    style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: primaryColor),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(post.body,
+                    style: const TextStyle(fontSize: 16, color: primaryColor)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
